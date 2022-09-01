19792d6ba15b05844659b84f86e8a5ccc30eb86a
Mirim <s2103@e-mirim.hs.kr>
2022-09-01 20:01:21 +0900

    내브 버텀 바 생성

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+        var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
+
+        bnv_main.run {
+            setOnNavigationItemReselectedListener {
+                when(it.itemId){
+                    R.id.item_fragment1 -> {
+                        val TodayGoTemple = TodayGoTemple()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, TodayGoTemple).commit()
+                    }
+                    R.id.item_fragment2 -> {
+                        val HomeFragment = HomeFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, HomeFragment).commit()
+                    }
+                    R.id.item_fragment3 -> {
+                        val WriteFragment = WriteFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, WriteFragment).commit()
+                    }
+                    R.id.item_fragment4 -> {
+                        val UserFragment = UserFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, UserFragment).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.item_fragment2
+        }
+
     }
 }
