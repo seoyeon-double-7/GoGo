f00a61d6c5408a88839210a41d12e64edc618f89
eunjin2118 <ally6324@naver.com>
2022-09-02 08:19:13 +0900

    회원가입버튼 로그인엑티비티로 연결

--- app/src/main/java/com/example/myapplication/SignUpActivity.kt
+++ app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,11 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         supportActionBar?.hide()
+
+        findViewById<Button>(R.id.signUp_btn).setOnClickListener {
+            Toast.makeText(this, "회원가입 되었습니다", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
