10f8b4527619e0f86056b5b0ee3e1f044b2bae8b
eunjin2118 <ally6324@naver.com>
2022-09-01 20:04:03 +0900

    어댑터를 통해 스피너와 아이템 연결하기

--- app/src/main/java/com/example/myapplication/WriteFragment.kt
+++ app/src/main/java/com/example/myapplication/WriteFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,46 +19,21 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class WriteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        //  어댑터를 통해 스피너와 아이템 연결하기
-//        val sAdapter = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_dropdown_item);
-//        categoryComboBox.setAdapter(sAdapter);
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_write, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write, container, false)
+        //  어댑터를 통해 스피너와 아이템 연결하기
+        val sAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.category, android.R.layout.simple_spinner_dropdown_item);
+        val categoryComboBox = view.findViewById<Spinner>(R.id.categoryComboBox)
+        categoryComboBox.setAdapter(sAdapter);
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WriteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WriteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
