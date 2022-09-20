2cdc7b908a81230dcfcc7de853b927585c2230e2
Mirim <s2103@e-mirim.hs.kr>
2022-09-21 08:10:13 +0900

    home fragment와 AllFragment 연결중

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,16 +1,24 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import com.example.myapplication.R
+import com.example.myapplication.tabfragment.AllFragment
 
 class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        //childFragmentManager.beginTransaction().add(R.id.all_frag_holder,AllFragment()).commit();
+
     }
 
     override fun onCreateView(
@@ -18,7 +26,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+//        val button = view?.findViewById<ImageButton>(R.id.all_btn)
+//
+//        button?.setOnClickListener { view ->
+//            Log.d("mytag", "Selected")
+//        }
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
 
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
