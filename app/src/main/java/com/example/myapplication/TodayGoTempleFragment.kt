f2a1c85f2a3588ab5e359d2039322b68d1d141bf
eunjin2118 <ally6324@naver.com>
2022-10-13 19:00:46 +0900

    버튼으로 수정, onClickListener 진행중

--- app/src/main/java/com/example/myapplication/TodayGoTempleFragment.kt
+++ app/src/main/java/com/example/myapplication/TodayGoTempleFragment.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class TodayGoTemple : Fragment() {
@@ -18,8 +20,13 @@ class TodayGoTemple : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today_go_temple, container, false)
+//        var view = inflater.inflate(R.layout.fragment_today_go_temple, container, false)
+//
+//        view.findViewById<Button>(R.id.btn_career).setOnClickListener {
+//            val intent = Intent(activity, )
+//        }
+
+        return view
     }
 
 }
