6abaad8780417709bb04784a511300fca5a912dc
Mirim <s2103@e-mirim.hs.kr>
2022-10-17 08:18:43 +0900

    오늘의 Go 전당 포스트 박스 레이아웃

--- app/src/main/java/com/example/myapplication/tabfragment/SchoolFragment.kt
+++ app/src/main/java/com/example/myapplication/tabfragment/SchoolFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.PostListItem
 import com.example.myapplication.R
+import com.example.myapplication.com.example.myapplication.PostListAdapter
 
 class SchoolFragment : Fragment() {
+
+    val postItemList = arrayListOf<PostListItem>()
+    val postListAdapter = PostListAdapter(postItemList)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_school, container, false)
+        val view =  inflater.inflate(R.layout.fragment_school, container, false)
+
+        val dataList = view?.findViewById<RecyclerView>(R.id.post_list)
+
+        dataList?.layoutManager = LinearLayoutManager(requireContext())
+        dataList?.adapter = postListAdapter
+
+        postItemList.add(PostListItem("일서연","학교생활", "성적 관련 질문", "전과목 평균 80이면 몇등급 나오나용??"))
+        postItemList.add(PostListItem("이서연","학교생활", "성적 관련 질문", "전과목 평균 80이면 몇등급 나오나용??"))
+        postItemList.add(PostListItem("삼서연","학교생활", "성적 관련 질문", "전과목 평균 80이면 몇등급 나오나용??"))
+        postItemList.add(PostListItem("사서연","학교생활", "성적 관련 질문", "전과목 평균 80이면 몇등급 나오나용??"))
+
+        postListAdapter.notifyDataSetChanged()
+
+        return view
     }
 }
